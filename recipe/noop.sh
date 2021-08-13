2932ef287b2d5c20715581061b228c27d1e94256
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-08-13 13:57:30 +0200

    make noop variant pkg_resources compatible

--- recipe/noop.sh
+++ recipe/noop.sh
@@ -1,1 +1,16 @@
-$PYTHON --version
+cat <<EOF > setup.py
+import os
+from setuptools import setup
+
+
+version = os.environ['PKG_VERSION']
+
+setup(name="dataclasses",
+      version=version,
+      platforms=["any"],
+      python_requires='>=3.7',
+      description='Empty dataclasses shim to make it visible for pkg_resources under Python 3.7+'
+)
+EOF
+
+$PYTHON -m pip install --no-deps --ignore-installed .
